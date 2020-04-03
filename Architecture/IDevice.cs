91e0978cc6dece68b4baf76a07b7eed5063f095b
Quentin Arguillere <arguillq@gmail.com>
2020-04-03 23:14:53 +0200

    Testing some gi

---
+++ Architecture/IDevice.cs
@@ -0,0 +1,38 @@
+﻿
+struct DeviceCommand
+{
+};
+
+// Ids will be built and attributed by the MasterDevice
+using DeviceId = uint;
+
+
+interface IDevice
+{
+    DeviceId getDeviceId();
+    void setDeviceId(DeviceId newId);
+    void processCommand(DeviceCommand command);
+};
+
+interface IMasterDevice
+{
+    // Top level management of the registered devices list
+    list<IDevice*> detectNearbyDevices();
+    map<DeviceId, IDevice*> * getRegisteredDevices();
+
+    // Send a command to every registered device
+    void broadcastCommand(DeviceCommand command);
+
+    // Send a command to a specific device
+    void sendCommand(DeviceCommand command, DeviceId id);
+};
+
+
+interface ITeamDevice :  IDevice
+{
+};
+
+
+interface IPlayerDevice :  IDevice
+{
+};
