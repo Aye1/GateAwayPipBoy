d11228cf1a31d16501ba98d02530c14619b8991f
Arguillere <arguillq@gmail.com>
2020-05-12 14:05:25 +0200

    Update to unity 3.13, create MovingBalanceBar folders and objects

---
+++ Assets/Scripts/Games/BarBalanceGame/MovingBalanceBar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovingBalanceBar : MonoBehaviour
+{
+   
+    public Image bar;
+
+    public int maxHeight = 100;
+    public float currentHeight = 0; // When == 100, bar is entirely filled
+    public float decreaseSpeed = 1;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        currentHeight = Math.Max(0, currentHeight - Time.deltaTime * decreaseSpeed);
+        Vector3 targetScale = new Vector3(1, currentHeight / (float)maxHeight, 1);
+        bar.transform.localScale = targetScale;
+        // bar.transform.localScale = Vector3.MoveTowards(bar.transform.localScale, targetScale, 1);
+    }
+
+}
